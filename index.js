4d096f02cad64e30275f67cef083cd9f57d7830c
Brian Thomas <btdeveloper125@gmail.com>
2021-07-05 10:29:04 -0600

    This commit adds express.static

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//1
 const express = require('express'),
   morgan = require('morgan');
 const app = express();
 
+//5
 app.use(morgan('common'));
-
+//2
 const movies ={
   title:[
     {
@@ -36,11 +38,13 @@ const movies ={
   ]
 };
 app.get('/movies', function(req, res){res.json(movies.title);});
+//3
 app.get('/', (req, res)=>{res.send('<h1>This route exists!!</h1>');});
-
+//4
+app.use(express.static('public'));
+app.get('/documentation', (req, res)=>{res.sendFile(__dirname + '/public/documentation.html');});
 app.use((err, req, res, next)=>{
   console.log(err.stack);
   res.status(500).send('We have a problem');
 });
-
 app.listen(3000);
