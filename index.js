b7a5c9bfb010eab4dfdf2f25b8a4381f567e8b4b
Brian Thomas <btdeveloper125@gmail.com>
2021-07-05 19:15:26 -0600

    This commit remove commets

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-// 1
 const express = require('express'); const morgan = require('morgan')
 const app = express()
 const movies = {
@@ -47,13 +46,9 @@ const options = {
     res.set('x-timestamp', Date.now())
   }
 }
-// 5
 app.use(morgan('common'))
-// 2
 app.get('/movies', function (req, res) { res.json(movies.title) })
-// 3
 app.get('/', (req, res) => { res.send('<h1>This route exists!!</h1>') })
-// 4
 app.use(express.static('public', options))
 app.use((err, req, res, next) => {
   console.log(err.stack)
