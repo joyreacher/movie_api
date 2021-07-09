b6ad7cf3bf0dacd504be3a3baf441467e87e64d6
Brian Thomas <btdeveloper125@gmail.com>
2021-07-09 08:34:40 -0600

    This commit refactors the genre route

--- index.js
+++ index.js
@@ -43,11 +43,32 @@ app.get('/content/movie/details/:id', (req, res) => {
   res.send(db.content.movie[id])
 })
 // return a specific genre
-app.get('/content/movies?:genre', (req, res, next) => {
+/*
+  ?(/content/movies?:genre) --CONFLICTS WITH (content/movies)
+  !SWITCH FROM: req.query.genre
+  ?TO: req.params.genre
+*/
+app.get('/content/movies/:genre', (req, res) => {
   // get the value from the url
-  const genere = req.query.genre
+  let genre = req.params.genre
   // return the array of objects with title and url values
-  if (genere === 'action') res.send(db.content.genre.action)
+  switch(genre){
+    case 'comedy':
+      res.send(db.content.genre.comedy);
+      break;
+    case 'action':
+      res.send(db.content.genre.action);
+      break;
+    case 'sci-fi':
+      res.send(db.content.genre.sci-fi);
+      break;
+    case 'drama':
+      res.send(db.content.genre.drama);
+      break;
+    case 'horror':
+      res.send(db.content.genre.horror);
+      break;
+  }
 })
 // return director
 app.get('/content/movies/filmography/:title/:name', (req, res) => {
