852442c65f71f7629714c5c6c8879df7dfebe325
Brian Thomas <btdeveloper125@gmail.com>
2021-07-16 12:18:46 -0600

    This commit adds a genres query, and an updated find movie using 2 queries for movies, genres collections

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 const Genre = Models.Genre;
+const Director = Models.Director;
 mongoose.connect('mongodb+srv://Brian:Takka__411@cluster0.ganu8.mongodb.net/myFlix?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 // mongodb+srv://Brian:<password>@cluster0.ganu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const express = require('express'); const morgan = require('morgan')
@@ -34,21 +35,37 @@ app.get('/movies', (req, res)=>{
   Movies.find().then(title=>res.json(title))
 })
 
+/* 
+  REQUEST: GET
+  RESPONSE: 
+*/
+app.get('/movies/:title', (req, res)=>{
+  Movies.findOne({Title: req.params.title})
+  .then((movie)=>{
+      Genre.findOne({Name: movie.Genre.Name[0]})
+      .populate('Movie').exec((err, genre) => {
+        Object.keys(movie.Genre).forEach(function(key){
+          //movie.Genre[key]
+          //genre[key]
+          if(movie.Genre[key] != genre.Name){
+            console.log("👏 ")
+            movie.Genre[key] = genre[key]
+          }
+        })
+        res.json(movie)
+    })
+  })
+  .catch((error)=>{
+    console.log(error)
+    res.status(500).send('Error: ' + error)
+  })
+})
+
+//GET ALL GENRES
 app.get('/genres', (req, res)=>{
   Genre.find().then(genre=>res.json(genre))
 })
 
-//GET A MOVIE BY TITLE
-app.get('/movies/:title', (req, res)=>{
-  Movies.findOne({Title: req.params.title})
-    .then((movie)=>{
-      res.json(movie)
-    })
-    .catch((error)=>{
-      console.log(error)
-      res.status(500).send('Error: ' + error)
-    })
-})
 //GET A LIST OF MOVIES BY GENRE
 app.get('/genre/:genre', (req, res)=>{
   Movies.find({"Genre.Name": req.params.genre})
@@ -59,6 +76,29 @@ app.get('/genre/:genre', (req, res)=>{
       res.status(500).send('Error: ' + err)
     })
 })
+
+//RETURNS ALL DIRECTORS
+app.get('/directors', (req, res)=>{
+  Director.find().then(director=>res.json(director))
+})
+
+app.get('/directors/:name', (req, res)=>{
+  Director.find({"Name": req.params.name}).then(director=>res.json(director))
+})
+
+// app.get('/directors/:name', (req, res)=>{
+  
+//   //TODO remove Movies and use Director.
+//   Movies.find({"Director.Name": req.params.name})
+//     .then((director)=>{
+      
+//       //TODO response will be different
+//       res.json(director[0].Director)
+//     })
+//     .catch((err)=>{
+//       res.status(500).send('Error: ' + err)
+//     })
+// })
 
 app.post('/movies', (req, res)=>{
   console.log(req.body)
@@ -79,9 +119,36 @@ app.post('/movies', (req, res)=>{
   })
   // res.send('POST create a movie resource.')
 })
-
-app.put('/movies/:movieid', (req, res)=>{
-  res.send('UPDATE a movie resource.')
+//! USED TO UPDATE TO REMOVE GENRE OBJECT
+app.put('/movies/:title', (req, res)=>{
+  console.log(req.body)
+  console.log(req.params)
+  Movies.findOneAndUpdate( {title: req.body.title }, {$set:
+      {
+        Title: req.params.title,
+        Description: "During a nuclear test, the French government inadvertently mutates a lizard nest; years later, a giant lizard makes its way to New York City. Dr. Niko Tatopoulos, an expert on the effects of radiation on animals, is sent by the U.S. government to study the beast. When the creature, dubbed 'Godzilla' by news outlets, emerges, a massive battle with the military begins. To make matters worse, Niko discovers that Godzilla has laid a nest of 200 eggs, which are ready to hatch.",
+        Genre: {},
+        Director: {
+          Name: "Roland Emmerich",
+          Bio: "Roland Emmerich (German pronunciation: [ˈʁoːlant ˈɛməʁɪç] (About this soundlisten); born 10 November 1955) is a German film director, screenwriter and producer, widely known for his science fiction films. His films, most of which are English-language Hollywood productions, have made more than $3 billion worldwide, including just over $1 billion in the United States, making him the country's 15th-highest-grossing director of all time.[1][2] He began his work in the film industry by directing the film The Noah's Ark Principle (1984) as part of his university thesis and also co-founded Centropolis Entertainment in 1985 with his sister. He is also known for directing the films such as Universal Soldier (1992), Stargate (1994), Independence Day (1996), Godzilla (1998), The Patriot (2000), The Day After Tomorrow (2004), 2012 (2009), and most recently, Midway (2019). He is a collector of art and an active campaigner for the LGBT community, and is openly gay.[3]",
+          Birth: "1995",
+          Death: ""
+        },
+        ImagePath:"godzilla.png",
+        Featured: false
+        
+      }
+    },
+    { new: true },
+    (err, updatedMovie)=>{
+      if(err){
+        console.log(err)
+        res.status(500).send('Error:' + err)
+      }else{
+        res.json(updatedMovie)
+      }
+    }
+  )
 })
 
 app.delete('/movies/:movieid', (req, res)=>{
