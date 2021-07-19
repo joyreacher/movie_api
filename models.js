784aa2fa6b746bea1c7c363f8a1f2f5a1af65739
Brian Thomas <btdeveloper125@gmail.com>
2021-07-19 14:07:41 -0600

    This commt removes the director schema and refactors the genre schema

--- models.js
+++ models.js
@@ -1,77 +1,32 @@
 const mongoose = require('mongoose')
-const { Schema } = mongoose;
 
-const genreSchema = new Schema({
-  Name: [{
-    type: mongoose.Schema.Types.String,
-    ref: 'Genre'
-  }],
-  Description: [{
-    type: mongoose.Schema.Types.String,
-    ref: 'Genre'
+const genreSchema = new mongoose.Schema({
+  Name: String,
+  Description: String
+})
+
+const movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: String,
+  Genre: genreSchema,
+  Director: { Name: String, Bio: String, Birth: Date, Death: Date },
+  ImagePath: String,
+  Featured: Boolean
+}, {versionKey: false})
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthday: Date,
+  favorite_movies: [{
+    _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
+    Title: String
   }]
 })
 
-const directorSchema = new Schema({
-  // Name: String,
-  Name: {
-    type: mongoose.Schema.Types.String,
-    ref: 'Director'
-  },
-  // Bio: String, 
-  Bio: {
-    type: mongoose.Schema.Types.String,
-    ref: 'Director'
-  },
-  Birth: Date,
-  Death: Date
-})
+const Movie = mongoose.model('Movie', movieSchema, 'movies')
+const User = mongoose.model('User', userSchema, 'users')
 
-let movieSchema = mongoose.Schema({
-  
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
-  Genre: {
-    Name: {
-      type: mongoose.Schema.Types.String,
-      ref: 'Genre'
-    },
-    Description: {
-      type: mongoose.Schema.Types.String,
-      ref: 'Genre'
-    } 
-  },
-  Director: {
-    // Name: String,
-    Name: {
-      type: mongoose.Schema.Types.String,
-      ref: 'Director'
-    },
-    // Bio: String
-    Bio: {
-      type: mongoose.Schema.Types.String,
-      ref: 'Director'
-    },
-  },
-  Actors: [String],
-  ImagePath: String,
-  Featured: Boolean
-});
-
-let userSchema = mongoose.Schema({
-  username: {type: String, required: true},
-  password: {type: String, required: true},
-  email: {type: String, required: true},
-  birthday: Date,
-  favorite_movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-});
-
-let Movie = mongoose.model('Movie', movieSchema, 'movies');
-let User = mongoose.model('User', userSchema);
-let Genre = mongoose.model('Modal', genreSchema, 'genres');
-let Director = mongoose.model('Director', directorSchema, 'directors');
-
-module.exports.Movie = Movie;
-module.exports.User = User;
-module.exports.Genre = Genre;
-module.exports.Director = Director;
+module.exports.Movie = Movie
+module.exports.User = User
