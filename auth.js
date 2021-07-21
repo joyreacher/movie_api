27902585c2494f3f5cd33c87c6e29ba79d508637
Brian Thomas <btdeveloper125@gmail.com>
2021-07-21 12:01:19 -0600

    This commit sets up the login endpoint to validate the jwt

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 //This has to be the same key used in the JWTStrategy
 const jwtSecret = 'your_jwt_secret'; 
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+const jwt = require('jsonwebtoken');
+passport = require('passport');
 
 // Your local passport file
 require('./passport');
@@ -24,8 +24,7 @@ let generateJWTToken = (user) =>{
 
 module.exports = (router) => {
   router.post('/login', (req, res)=>{
-    passport.authenticate('local', { session: false
-    }, (error, user, info) =>{
+    passport.authenticate('local', { session: false}, (error, user, info) =>{
       if(error || !user){
         return res.status(400).json({
           message: 'Something is not right',
@@ -36,9 +35,9 @@ module.exports = (router) => {
         if(error){
           res.send(error);
         }
-      let token = generateJWTToken(user.toJSON());
-      // Return the token
-      return res.json({ user, token })
+        let token = generateJWTToken(user.toJSON());
+        // Return the token
+        return res.json({ user, token })
       });
     })(req, res);
   })
