54e26fd7e60a37b443a29f7c909bf2462d890a68
Brian Thomas <btdeveloper125@gmail.com>
2021-07-21 12:00:22 -0600

    This commit fixes the secreOrKey value

--- passport.js
+++ passport.js
@@ -1,11 +1,13 @@
-const passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy,
-Models = require('./models.js'),
+const passport = require('passport');
+LocalStrategy = require('passport-local').Strategy;
+Models = require('./models.js');
 passportJWT = require('passport-jwt');
 
-let Users = Models.User,
-JWTStrategy = passportJWT.Strategy,
+let Users = Models.User;
+JWTStrategy = passportJWT.Strategy;
 ExtractJWT = passportJWT.ExtractJwt;
+
+
 
 
 passport.use( new LocalStrategy({
@@ -30,14 +32,16 @@ passport.use( new LocalStrategy({
 
 passport.use( new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'Your_jwt_Secret'
-}, (jwtPayload, callback)=>{
-  return Users.findById(jwtPayload._id)
-    .then((user)=>{
-      return callback(null, user)
-    })
-    .catch((error)=>{
-      return callback(error)
-    })
-}
+  secretOrKey: 'your_jwt_secret'
+  }, 
+  (jwtPayload, callback)=>{
+      return Users.findById(jwtPayload._id)
+      .then((user)=>{
+        console.log(user)
+        return callback(null, user)
+      })
+      .catch((error)=>{
+        return callback(error)
+      })
+  }
 ));
